14e49b0cd261cda3e2b8733da79ca3ba1e734776
Narang Chouhan <narangchouhan@users.noreply.github.com>
2025-07-15 16:49:34 +0530

    first commit

--- main.py
+++ main.py
@@ -85,3 +85,6 @@ def index():
         return "OK", 200
     return "Bot is running!", 200
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
